2db99c7b1f9a4c200e68d0a1786fafb0daa95b55
Pedro Martins <pedrogmhead@gmail.com>
2021-02-16 19:46:03 -0300

    added text formating

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,11 +4,26 @@ import 'package:flutter/material.dart';
 void main() { //obrigatório para a aplicação
   runApp(MaterialApp( //necessária após main para ser o app, aqui vai o conteúdo
     title: "Frases do dia", //título como se fosse html
-    home: Row(children: <Widget>[
-      Text("T1"),
-      Text(" T2"),
-      Text(" T3")
-    ],), //faça a inserção com children
-    //home: Container(color: Colors.white,), // container como no bootstrap
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+              "Lorem ipsum ",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 50,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.normal,
+              letterSpacing: 0,
+              wordSpacing: 0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+              decorationColor: Colors.purple
+            ),
+          )
+        ], //children: pode conter vários itens
+      ), //child = pode conter apenas um item (column/row);
+    ),
   ));
 }
