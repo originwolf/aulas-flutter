9f5d75341c8d40802741aa5195d08f4c585f4a3d
Pedro Martins <pedrogmhead@gmail.com>
2021-02-18 20:51:02 -0300

    added magins and paddings

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,24 +3,34 @@ import 'package:flutter/material.dart';
 
 void main() { //obrigatório para a aplicação
   runApp(MaterialApp( //necessária após main para ser o app, aqui vai o conteúdo
+    //debugShowCheckedModeBanner: false, //ocultar faixa escrito debug
     title: "Frases do dia", //título como se fosse html
     home: Container(
-      color: Colors.white,
+      //color: Colors.white,
+      //padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
+      margin: EdgeInsets.fromLTRB(30, 50, 30, 30),
+      decoration: BoxDecoration(
+        border: Border.all(width: 3, color: Colors.white)
+      ),
       child: Column(
         children: <Widget>[
-          FlatButton(
-              onPressed: (){
-                print("Botão pressionado");
-              },
-              child: Text(
-                  "Clique aqui",
-                style: TextStyle(
-                  color: Colors.black,
-                  decoration: TextDecoration.none,
-                  fontSize: 20
-                ),
-              )
-          )
+          Text("T1"),
+
+          Padding(
+              padding: EdgeInsets.all(30),
+              child: Text("T2"),
+          ),
+
+          Text("T3")
+          /*Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed vestibulum est. Nulla dapibus neque erat",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 35,
+              decoration: TextDecoration.none
+            ),
+          )*/
         ], //children: pode conter vários itens
       ), //child = pode conter apenas um item (column/row);
     ),
