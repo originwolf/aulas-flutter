8a21601713622ae469871dd85a27c405fe4d536a
Pedro Martins <pedrogmhead@gmail.com>
2021-02-18 21:18:42 -0300

    axis alignment

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,38 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() { //obrigatório para a aplicação
-  runApp(MaterialApp( //necessária após main para ser o app, aqui vai o conteúdo
-    //debugShowCheckedModeBanner: false, //ocultar faixa escrito debug
-    title: "Frases do dia", //título como se fosse html
+void main() {
+  runApp(MaterialApp(
+    title: "Frases do dia",
     home: Container(
-      //color: Colors.white,
-      //padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
-      margin: EdgeInsets.fromLTRB(30, 50, 30, 30),
+      margin: EdgeInsets.only(top: 60),
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.white)
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Text("T1"),
-
-          Padding(
-              padding: EdgeInsets.all(30),
-              child: Text("T2"),
-          ),
-
+          Text("T2"),
           Text("T3")
-          /*Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed vestibulum est. Nulla dapibus neque erat",
-            textAlign: TextAlign.end,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 35,
-              decoration: TextDecoration.none
-            ),
-          )*/
-        ], //children: pode conter vários itens
-      ), //child = pode conter apenas um item (column/row);
+        ],
+      ),
     ),
   ));
 }
