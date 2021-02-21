a7804a5f1f98abc08aa2eb8654018d3fd0cf1854
Pedro Martins <pedrogmhead@gmail.com>
2021-02-21 01:07:11 -0300

    added images

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -9,14 +9,9 @@ void main() {
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.white)
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("T1"),
-          Text("T2"),
-          Text("T3")
-        ],
+      child: Image.asset(
+        "images/mesa.jpg",
+        fit: BoxFit.fitWidth,
       ),
     ),
   ));
