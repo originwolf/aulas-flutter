3d66ba98b6874d0b113a6b1afe53ced94bb80191
Pedro Martins <pedrogmhead@gmail.com>
2021-02-21 03:49:00 -0300

    added stateful and stateless

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,18 +1,100 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//stateless = widgets que não podem ser alterados; (constante)
+//stateful = widgets que podem ser alterados; (variável)
+
 void main() {
   runApp(MaterialApp(
-    title: "Frases do dia",
-    home: Container(
-      margin: EdgeInsets.only(top: 60),
-      decoration: BoxDecoration(
-        border: Border.all(width: 3, color: Colors.white)
-      ),
-      child: Image.asset(
-        "images/mesa.jpg",
-        fit: BoxFit.fitWidth,
-      ),
-    ),
+    debugShowCheckedModeBanner: false,
+    home: HomeStateful(),
   ));
+}
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _texto = "Instagram";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_texto),
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            RaisedButton(
+                onPressed: (){
+                  setState(() {
+                    _texto = "Facebook";
+                  });
+                },
+              color: Colors.purple,
+              textColor: Colors.white,
+              child: Text("Facebook"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  _texto = "Instagram";
+                });
+              },
+              color: Colors.purple,
+              textColor: Colors.white,
+              child: Text("Instagram"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  _texto = "WhatsApp";
+                });
+              },
+              color: Colors.purple,
+              textColor: Colors.white,
+              child: Text("WhatsApp"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("Conteúdo principal"),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.pinkAccent,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: <Widget>[
+              Text("Texto 1"),
+              Text("Texto 2"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
