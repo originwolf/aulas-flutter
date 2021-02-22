be6250420064fb2c991d796cfb6db01df91b21ff
Pedro Martins <pedrogmhead@gmail.com>
2021-02-22 01:34:35 -0300

    added challenge made my way

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,100 +1,42 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-//stateless = widgets que não podem ser alterados; (constante)
-//stateful = widgets que podem ser alterados; (variável)
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeStateful(),
-  ));
-}
-
-class HomeStateful extends StatefulWidget {
-  @override
-  _HomeStatefulState createState() => _HomeStatefulState();
-}
-
-class _HomeStatefulState extends State<HomeStateful> {
-
-  var _texto = "Instagram";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_texto),
-        backgroundColor: Colors.purpleAccent,
-      ),
-      body: Container(
-        child: Row(
-          children: <Widget>[
-            RaisedButton(
-                onPressed: (){
-                  setState(() {
-                    _texto = "Facebook";
-                  });
-                },
-              color: Colors.purple,
-              textColor: Colors.white,
-              child: Text("Facebook"),
-            ),
-            RaisedButton(
-              onPressed: (){
-                setState(() {
-                  _texto = "Instagram";
-                });
-              },
-              color: Colors.purple,
-              textColor: Colors.white,
-              child: Text("Instagram"),
-            ),
-            RaisedButton(
-              onPressed: (){
-                setState(() {
-                  _texto = "WhatsApp";
-                });
-              },
-              color: Colors.purple,
-              textColor: Colors.white,
-              child: Text("WhatsApp"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    var _titulo = "Instagram";
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_titulo),
-        backgroundColor: Colors.purpleAccent,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Text("Conteúdo principal"),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.pinkAccent,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: <Widget>[
-              Text("Texto 1"),
-              Text("Texto 2"),
-            ],
+    title: "Frases do Dia",
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text("Frases do Dia"),
+            backgroundColor: Colors.green,
           ),
-        ),
+          Padding(
+              padding: EdgeInsets.only(top: 55),
+            child: Image.asset("images/logo.png"),
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(16, 50, 16, 50),
+            child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sapien lorem, egestas et eros at, hendrerit elementum odio. Curabitur vulputate ante scelerisque arcu cursus cursus. Sed at eros rhoncus est faucibus pharetra. Fusce lacinia odio libero, sed hendrerit nisl accumsan facilisis. Pellentesque eu suscipit nisi. Aliquam in vestibulum mi.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+          FlatButton(
+              onPressed: (){
+                print("Estou funcionando!");
+              },
+            color: Colors.lightGreen,
+            textColor: Colors.white,
+              child: Text("Nova frase"),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  ));
 }
