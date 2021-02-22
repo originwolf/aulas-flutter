bb762a19b69ece4608e3bca0039c391f781b3755
Pedro Martins <pedrogmhead@gmail.com>
2021-02-22 02:00:43 -0300

    interface created by the professor

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,42 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Frases do Dia",
-    home: Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          AppBar(
-            title: Text("Frases do Dia"),
-            backgroundColor: Colors.green,
-          ),
-          Padding(
-              padding: EdgeInsets.only(top: 55),
-            child: Image.asset("images/logo.png"),
-          ),
-          Padding(
-              padding: EdgeInsets.fromLTRB(16, 50, 16, 50),
-            child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sapien lorem, egestas et eros at, hendrerit elementum odio. Curabitur vulputate ante scelerisque arcu cursus cursus. Sed at eros rhoncus est faucibus pharetra. Fusce lacinia odio libero, sed hendrerit nisl accumsan facilisis. Pellentesque eu suscipit nisi. Aliquam in vestibulum mi.",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-                decoration: TextDecoration.none,
-              ),
-            ),
-          ),
-          FlatButton(
-              onPressed: (){
-                print("Estou funcionando!");
-              },
-            color: Colors.lightGreen,
-            textColor: Colors.white,
-              child: Text("Nova frase"),
-          )
-        ],
-      ),
-    ),
+    home: Home(),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center( //centraliza o conteúdo no espaço disponível, sem que ele ocupe o width
+        child: Container(
+          padding: EdgeInsets.all(16),
+          //width: double.infinity, //abordagem para a conteúdo ocupar 100% do width da tela
+          /*decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.amber),
+          ),*/
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                "Clique abaixo para gerar uma frase",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black
+                ),
+              ),
+              RaisedButton(
+                onPressed: (){
+                  print("Funcionando");
+                },
+                child: Text(
+                  "Nova frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
