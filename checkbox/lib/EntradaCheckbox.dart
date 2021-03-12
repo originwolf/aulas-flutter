41c9dc0042c59f67771bb270c373fb8058b5845e
Pedro Martins <pedrogmhead@gmail.com>
2021-03-12 15:06:27 -0300

    added CheckboxListTile

--- checkbox/lib/EntradaCheckbox.dart
+++ checkbox/lib/EntradaCheckbox.dart
@@ -19,24 +19,44 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
       body: Container(
         child: Column(
           children: <Widget>[
-            Text("Comida Brasileira:"),
-            Checkbox(
-                value: _statusComidaBrasileira, //checkbox inicia com um valor booleano, e repassa esse booleano na função onchanged
+            CheckboxListTile(
+              title: Text("Comida Brasileira"),
+                subtitle: Text("A melhor comida do mundo!"),
+                activeColor: Colors.pink,
+                secondary: Icon(Icons.watch_later),
+                value: _statusComidaBrasileira,
                 onChanged: (bool valor){
                   setState(() {
                     _statusComidaBrasileira = valor;
                     _statusComidaMexicana = !valor;
                   });
-                }
+                },
             ),
-            Text("Comida Mexicana:"),
-            Checkbox(
-                value: _statusComidaMexicana,
-                onChanged: (bool valor){
-                  setState(() {
-                    _statusComidaMexicana = valor;
-                    _statusComidaBrasileira = !valor;
-                  });
+            CheckboxListTile(
+              title: Text("Comida Mexicana"),
+              subtitle: Text("Não é a melhor comida do mundo."),
+              activeColor: Colors.pink,
+              secondary: Icon(Icons.watch_later),
+              value: _statusComidaMexicana,
+              onChanged: (bool valor){
+                setState(() {
+                  _statusComidaMexicana = valor;
+                  _statusComidaBrasileira = !valor;
+                });
+              },
+            ),
+            RaisedButton(
+              color: Colors.pinkAccent,
+              textColor: Colors.white,
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+                onPressed: (){
+                  print("Comida Brasileira: ${_statusComidaBrasileira} - "
+                      "Comida Mexicana: ${_statusComidaMexicana}");
                 },
             ),
           ],
